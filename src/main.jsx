fef43e464744d162ab57520cb52333590a122c91
Your Name <you@example.com>
2023-10-16 20:41:40 +0600

    added a private routes

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import AuthProvider from './provider/AuthProvider';
+import Orders from './components/Orders';
+import PrivateRoutes from './routes/PrivateRoutes';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,10 @@ const router = createBrowserRouter([
       {
         path: "/register",
         element: <Register></Register>
+      },
+      {
+        path: "/orders",
+        element: <PrivateRoutes><Orders></Orders></PrivateRoutes>
       }
     ]
   },
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -10,17 +10,22 @@ export const AuthContext = createContext( null);
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
 
     const createUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
 
     const signInUser = (email, password) =>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const logOut = () =>{
+        setLoading(true)
         return signOut(auth)
     }
 
@@ -31,6 +36,7 @@ const AuthProvider = ({children}) => {
          const unSubscribe =   onAuthStateChanged(auth, currentUser =>{
                 console.log('current value of the current user ', currentUser);
                 setUser(currentUser)
+                setLoading(false)
             })
             return ()=>{
                 unSubscribe()
